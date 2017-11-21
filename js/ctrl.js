8c7ecb3e89921b3fbe0cf4860d9e1658bf2534c2
Bartek Dybała <dybalix@gmail.com>
2017-11-22 00:48:29 +0100

    add new customer

--- js/ctrl.js
+++ js/ctrl.js
@@ -84,12 +84,25 @@
                     console.log('UNABLE TO get customers!');
                 })
         }
+        $scope.addCustomer = function (customer) {
+            // adapt to dto
+            customer.identityCard.monthExpiring = new Date(customer.identityCard.idCardExpiring).getMonth();
+            customer.identityCard.yearExpiring = new Date(customer.identityCard.idCardExpiring).getYear();
+
+            dataFactory.addCustomer(customer)
+                .then(function (response) {
+                    // TODO dialog -> Data added
+                }, function (error) {
+                    // TODO dialog -> error
+                })
+        };
+
         function getRooms() {
             dataFactory.getRooms()
                 .then(function (response) {
                     $scope.rooms = response.data;
                 }, function (error) {
-                    console.log('UNABLE TO get rooms!');
+                    // TODO dialog -> error
                 })
         }
     }
